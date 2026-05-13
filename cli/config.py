89225b9b40c8c0dfc217cd4d2e68197a58d93a70
Gabriel Rodriguez <gabyrod7@gmail.com>
2026-05-13 23:27:50 +0200

    Add LLM config options for openai, anthropic and gemin

--- cli/config.py
+++ cli/config.py
@@ -1,7 +1,10 @@
+import os
+
 from dotenv import set_key
 from huggingface_hub import list_models
+from constants import PROVIDER_SPECS
 
-def config_command(model_name: str, list_model_names: bool):
+def config_command(model_name: str, list_model_names: bool, llm_provider: str, llm_model_name: str, llm_api_key: str):
     model_list = list_models(author="Helsinki-NLP")
 
     if list_model_names:
@@ -16,7 +19,6 @@ def config_command(model_name: str, list_model_names: bool):
         model_found = False
         for model in model_list:
             if model_name == model.id:
-                print(model_name)
                 model_found = True
 
         if not model_found:
@@ -25,3 +27,31 @@ def config_command(model_name: str, list_model_names: bool):
         set_key('.env', 'MODEL_NAME', model_name)
         print(f"Default model set to {model_name}")
 
+    if llm_provider:
+        supported_providers = tuple(PROVIDER_SPECS.keys())
+        if llm_provider not in supported_providers:
+            print(f"The LLM provider '{llm_provider}' is not supported. Please choose one from the list of supported providers:")
+            for provider in supported_providers:
+                print(provider)
+
+            while llm_provider not in supported_providers:
+                llm_provider = input('Choose provider: ')
+
+                if llm_provider not in supported_providers:
+                    print(f"The provider '{llm_provider}' is not supported. Please choose among the supported providers.")
+
+        set_key('.env', 'LLM_PROVIDER', llm_provider)
+        print('LLM_PROVIDER set to', llm_provider)
+
+    if llm_model_name:
+        if not llm_provider:
+            llm_provider = os.environ.get('LLM_PROVIDER')
+        set_key('.env', PROVIDER_SPECS[llm_provider]['model_env'], llm_model_name)
+        print(f"{PROVIDER_SPECS[llm_provider]['model_env']} has been set to {llm_model_name}")
+
+    if llm_api_key:
+        if not llm_provider:
+            llm_provider = os.environ.get('LLM_PROVIDER')
+        llm_api_key = os.environ.get(PROVIDER_SPECS[llm_provider]['api_key_env'])
+        set_key('.env', PROVIDER_SPECS[llm_provider]['api_key_env'], llm_api_key)
+        print(f"{PROVIDER_SPECS[llm_provider]['api_key_env']} has been set to {llm_api_key}")
